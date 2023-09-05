c05c6f9552baa5dda11ffa43112ef06a4c74a621
Luciane <lucimlozano@gmail.com>
2023-09-05 19:59:56 -0300

    Ajuste de estruturaçao padrao

--- gastsix/src/main.tsx
+++ gastsix/src/main.tsx
@@ -1,31 +1,10 @@
 //Imports React - adicionar imports dos componentes React abaixo 
-import React, { useEffect } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Link, BrowserRouter, Routes, Route } from 'react-router-dom'
 
 //Estilização da página CSS
 import './index.css'
 
-<<<<<<< HEAD
-//Import do Header
-import Header from '../src/Componentes/Header/index'
-
-//Imports de Pages - adicionar imports pages abaixo
-import Log_home from '../src/Componentes/Pages/Log_home/index'
-
-//Import de Assets - adicionar import de assets utilizadas na página main - login
-import './assets/img/LOGO.png'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <>
-    <BrowserRouter>
-    <Header></Header>
-    </BrowserRouter>
-    </>
-    
-  </React.StrictMode>,
-=======
 //componentes
 import Header from "../src/componentes/Header";
 import Menu_Lateral from "./componentes/Menu_Lateral";
@@ -43,9 +22,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Header />
       <Menu_Lateral />
       <Routes>{/*Indica uma lista de rotas*/}
-        <Route path='/Cad_Produto' element={<Cad_Produto />} /> {/*Indica o caminho do componente e o nome da rota dele*/}
+        <Route path='/cadastro/produto' element={<Cad_Produto />} /> {/*Indica o caminho do componente e o nome da rota dele*/}
       </Routes>
-      </BrowserRouter>
+    </BrowserRouter>
   </React.StrictMode>
->>>>>>> cad_produto
 )
