afe2cf07ff65d4ea1fa2004c7d0fb1eafb9c80e5
Luciane <lucimlozano@gmail.com>
2023-09-04 21:28:00 -0300

    padrão header e Cad_Produto

--- gastsix/src/main.tsx
+++ gastsix/src/main.tsx
@@ -2,9 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+//componentes
+import Header from "../src/componentes/Header";
+import Menu_Lateral from "./componentes/Menu_Lateral";
+import Cad_Produto from "./pages/Cad_Produto";
+
+//estilização global
+import "./index.css";
+
+//rotas
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-
-  </React.StrictMode>,
+    <BrowserRouter> {/*Indica que aplicação terá rotas*/}
+      <Header />
+      <Menu_Lateral />
+      <Routes>{/*Indica uma lista de rotas*/}
+        <Route path='/Cad_Produto' element={<Cad_Produto />} /> {/*Indica o caminho do componente e o nome da rota dele*/}
+      </Routes>
+      </BrowserRouter>
+  </React.StrictMode>
 )
