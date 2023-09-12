9536ac4b1d8c648015545610b2d76430a744ffad
Felipe <felipedefreitas177@gmail.com>
2023-09-12 20:30:51 -0300

    merge Rel_Movimentacao

--- gastsix/src/main.tsx
+++ gastsix/src/main.tsx
@@ -11,16 +11,10 @@ import Menu_Lateral from "./componentes/Menu_Lateral";
 import Cad_Produto from "./pages/Cad_Produto";
 import Cad_Fornecedores from './pages/Cad_Fornecedores';
 import Tel_Ajuste from "./pages/Tel_Ajuste";
-<<<<<<< HEAD
 import Ent_Estoque from "./pages/Ent_Estoque";
 import Con_Estoque from "./pages/Ent_Estoque";
 import Cad_Dispositivo from './pages/Cad_Dispositivo';
-=======
 import Rel_Movimentacao from './pages/Rel_Movimentacao';
-
-//estilização global
-import "./index.css";
->>>>>>> rel_movimentacao
 
 //rotas
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -34,13 +28,10 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path='/cadastroProduto' element={<Cad_Produto />} /> {/*Indica o caminho do componente e o nome da rota dele*/}
         <Route path='/cadastroFornecedores' element={<Cad_Fornecedores />} />
         <Route path='/telaAjuste' element={<Tel_Ajuste />} />
-<<<<<<< HEAD
         <Route path='/entEstoque' element={<Ent_Estoque />} />
         <Route path='/conEstoque' element={<Con_Estoque />} />
         <Route path='/cadastroDispositivo' element={<Cad_Dispositivo />} />
-=======
         <Route path='/relatorioMovimentacao' element={<Rel_Movimentacao />} />
->>>>>>> rel_movimentacao
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
