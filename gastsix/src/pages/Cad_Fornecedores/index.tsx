20e41f4c061dd84855e8d1c6f3f7971fafe2f863
Luciane <lucimlozano@gmail.com>
2023-09-11 21:30:17 -0300

    Realizado o useState e mascara do CNPJ na tela cad_fornecedores

--- gastsix/src/pages/Cad_Fornecedores/index.tsx
+++ gastsix/src/pages/Cad_Fornecedores/index.tsx
@@ -1,47 +1,106 @@
 import "./style.css";
 
+//Hook
+import { useState } from "react";
+
 function Cad_Fornecedores() {
-    return(
-        <main className="banner">
-        {/*indica o conteudo principal*/}
-        <section className="section__formulario">
-          {/*tag section indica uma secao*/}
-          <form className="formulario-central">
-            <div className="div__alinhamento_campos">
-              <label htmlFor="input__id">Id:</label>
-              <input type="text" name="input__id" id="" />
-            </div>
-            <div className="div__alinhamento_campos">
-              <label htmlFor="input__nome">Nome:</label>
-              <input type="text" name="input__nome" id="" />
-            </div>
-            <div className="div__alinhamento_campos">
-              <label htmlFor="input__cnpj">CNPJ:</label>
-              <input type="text" name="input__cnpj" id="" />
-            </div>
-            <div className="div__alinhamento_campos">
-              <label htmlFor="input__ie">IE:</label>
-              <input type="text" name="input__ie" id="" />
-            </div>
-            <div className="div__alinhamento_campos">
-              <label htmlFor="input__rs">Razão Social:</label>
-              <input type="text" name="input__rs" id="" />
-            </div>
-            <div className="div__alinhamento_campos">
-              <label htmlFor="input__E-mail">E-mail:</label>
-              <input type="email" name="input__E-mail" id="" />
-            </div>
-            <div className="div__alinhamento_campos">
-              <label htmlFor="input__end">Endereço:</label>
-              <input type="text" name="input__end" id="" />
-            </div>
-            <button className="botao_cadastrar" type="submit">
-              Cadastrar
-            </button>
-          </form>
-        </section>
-      </main>
-      
-    )
+  const [id, setId] = useState<string>("");
+  const [nome, setNome] = useState<string>("");
+  const [cnpj, setCnpj] = useState<string>("");
+  const [ie, setIe] = useState<string>("");
+  const [razaosocial, setRazaosocial] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [endereco, setEndereco] = useState<string>("");
+
+  function mascaraCNPJ(event: any) {
+    console.log(event.target.value);
+
+    // Remove todos os caracteres não-alfanuméricos
+    let cnpj = event.target.value.replace(/[^a-zA-Z0-9]/g, "");
+
+    // Aplica a máscara
+    cnpj = cnpj.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5");
+
+    // Atualiza o valor do campo CNPJ com a máscara aplicada
+    event.target.value = cnpj;
+
+  }
+  return (
+    <main className="banner">
+      {/*indica o conteudo principal*/}
+      <section className="section__formulario">
+        {/*tag section indica uma secao*/}
+        <form className="formulario-central">
+          <div className="div__alinhamento_campos">
+            <label htmlFor="input__id">Id:</label>
+            <input
+              type="text"
+              name="input__id"
+              id=""
+              onChange={(e) => setId(e.target.value)}
+            />
+          </div>
+          <div className="div__alinhamento_campos">
+            <label htmlFor="input__nome">Nome:</label>
+            <input
+              type="text"
+              name="input__nome"
+              id=""
+              onChange={(e) => setNome(e.target.value)}
+            />
+          </div>
+          <div className="div__alinhamento_campos">
+            <label htmlFor="input__cnpj">CNPJ:</label>
+            <input
+              type="text"
+              name="input__cnpj"
+              id=""
+              onChange={(e) => setCnpj(e.target.value)}
+            />
+          </div>
+          <div className="div__alinhamento_campos">
+            <label htmlFor="input__ie">IE:</label>
+            <input
+              type="text"
+              name="input__ie"
+              id=""
+              onChange={(e) => setIe(e.target.value)}
+            />
+          </div>
+          <div className="div__alinhamento_campos">
+            <label htmlFor="input__rs">Razão Social:</label>
+            <input
+              type="text"
+              name="input__rs"
+              id=""
+              onChange={(e) => setRazaosocial(e.target.value)}
+            />
+          </div>
+          <div className="div__alinhamento_campos">
+            <label htmlFor="input__E-mail">E-mail:</label>
+            <input
+              type="email"
+              name="input__E-mail"
+              id=""
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="div__alinhamento_campos">
+            <label htmlFor="input__end">Endereço:</label>
+            <input
+              type="text"
+              name="input__end"
+              id=""
+              onChange={(e) => setEndereco(e.target.value)}
+            />
+          </div>
+          <button className="botao_cadastrar" type="submit">
+            Cadastrar
+          </button>
+        </form>
+      </section>
+    </main>
+
+  )
 }
 export default Cad_Fornecedores;
