6a282e4dbf9cf35835fab71debe51291f3f3d702
Luciane <lucimlozano@gmail.com>
2023-09-11 21:51:11 -0300

    useState da tela cad_produto

--- gastsix/src/pages/Cad_Produto/index.tsx
+++ gastsix/src/pages/Cad_Produto/index.tsx
@@ -1,6 +1,16 @@
 import "./style.css"
 
+//Hook
+import { useState } from "react";
+
 function Cad_Produtos() {
+  const [descricao, setDescricao] = useState<string>("");
+  const [partnumber, setPartnumber] = useState<string>("");
+  const [codigoalternativo, setCodigoAlternativo] = useState<string>("");
+  const [codigofornecedor, setCodigoFornecedor] = useState<string>("");
+  const [codigosap, setCodigoSap] = useState<string>("");
+
+
   return (
     <main className="banner">
       {/*indica o conteudo principal*/}
@@ -9,23 +19,48 @@ function Cad_Produtos() {
         <form className="formulario-central">
           <div className="div__alinhamento_campos">
             <label htmlFor="input__descricao">Descrição:</label>
-            <input type="text" name="input__discricao" id="" />
+            <input
+              type="text"
+              name="input__discricao"
+              id=""
+              onChange={(e) => setDescricao(e.target.value)}
+            />
           </div>
           <div className="div__alinhamento_campos">
             <label htmlFor="input__part">Part Number:</label>
-            <input type="text" name="input__pat" id="" />
+            <input
+              type="text"
+              name="input__pat"
+              id=""
+              onChange={(e) => setPartnumber(e.target.value)}
+            />
           </div>
           <div className="div__alinhamento_campos">
             <label htmlFor="input__codigoalt">Código Alternativo:</label>
-            <input type="text" name="input__codigoalt" id="" />
+            <input
+              type="text"
+              name="input__codigoalt"
+              id=""
+              onChange={(e) => setCodigoAlternativo(e.target.value)}
+            />
           </div>
           <div className="div__alinhamento_campos">
             <label htmlFor="input__codigofor">Código Fornecedor:</label>
-            <input type="text" name="input__codigofor" id="" />
+            <input
+              type="text"
+              name="input__codigofor"
+              id=""
+              onChange={(e) => setCodigoFornecedor(e.target.value)}
+            />
           </div>
           <div className="div__alinhamento_campos">
             <label htmlFor="input__codigosap">Código SAP:</label>
-            <input type="text" name="input__codigosap" id="" />
+            <input
+              type="text"
+              name="input__codigosap"
+              id=""
+              onChange={(e) => setCodigoSap(e.target.value)}
+            />
           </div>
           <button className="botao_cadastrar" type="submit">
             Cadastrar
