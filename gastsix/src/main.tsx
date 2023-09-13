284231d36ec8b727f38c37884b5f1ff4f44bc022
Felipe <felipedefreitas177@gmail.com>
2023-09-12 21:17:17 -0300

    Ajustes das Rotas

--- gastsix/src/main.tsx
+++ gastsix/src/main.tsx
@@ -11,21 +11,13 @@ import Menu_Lateral from "./componentes/Menu_Lateral";
 import Cad_Produto from "./pages/Cad_Produto";
 import Cad_Fornecedores from './pages/Cad_Fornecedores';
 import Tel_Ajuste from "./pages/Tel_Ajuste";
-<<<<<<< HEAD
 import Ent_Estoque from "./pages/Ent_Estoque";
 import Con_Estoque from "./pages/Ent_Estoque";
 import Cad_Dispositivo from './pages/Cad_Dispositivo';
-<<<<<<< HEAD
 import Rel_Movimentacao from './pages/Rel_Movimentacao';
-=======
 import Rel_Operadores from './pages/Rel_Operadores';
-
-//estilização global
-import "./index.css";
->>>>>>> rel_operadores
-=======
 import Rel_Estoque from './pages/Rel_Estoque';
->>>>>>> rel_estoque
+
 
 //rotas
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -39,18 +31,12 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path='/cadastroProduto' element={<Cad_Produto />} /> {/*Indica o caminho do componente e o nome da rota dele*/}
         <Route path='/cadastroFornecedores' element={<Cad_Fornecedores />} />
         <Route path='/telaAjuste' element={<Tel_Ajuste />} />
-<<<<<<< HEAD
-        <Route path='/entEstoque' element={<Ent_Estoque />} />
-        <Route path='/conEstoque' element={<Con_Estoque />} />
+        <Route path='/entradaEstoque' element={<Ent_Estoque />} />
+        <Route path='/consultaEstoque' element={<Con_Estoque />} />
         <Route path='/cadastroDispositivo' element={<Cad_Dispositivo />} />
-<<<<<<< HEAD
         <Route path='/relatorioMovimentacao' element={<Rel_Movimentacao />} />
-=======
         <Route path='/relatorioOperadores' element={<Rel_Operadores />} />
->>>>>>> rel_operadores
-=======
         <Route path='/relatorioEstoque' element={<Rel_Estoque />} />
->>>>>>> rel_estoque
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
