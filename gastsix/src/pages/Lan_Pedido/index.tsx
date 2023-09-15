4210c9c89e8a5d950fd755c805740a0a8e936f40
cicero <ciceroqlima@gmail.com>
2023-09-15 19:14:38 -0300

    usestate e required da tela lan_pedido

--- gastsix/src/pages/Lan_Pedido/index.tsx
+++ gastsix/src/pages/Lan_Pedido/index.tsx
@@ -1,6 +1,16 @@
 import "./style.css"
 
+//Hook
+import { useState } from "react";
+
 function Lan_Pedido() {
+
+  const [CoddoProduto, setCodDoProduto] = useState<string>("");
+  const [NumPedido, setNumPedido] = useState<string>("");
+  const [Operador, setOperador] = useState<string>("");
+  const [Supervisor, setSupervisor] = useState<string>("");
+  const [Justificativa, setJustificativa] = useState<string>("");
+
   return (
     <main className="banner">
       {/*indica o conteudo principal*/}
@@ -9,23 +19,53 @@ function Lan_Pedido() {
         <form className="formulario-central">
           <div className="div__alinhamento_campos">
             <label htmlFor="input__cod">Cod.do Produto:</label>
-            <input type="text" name="input__cod" id="" />
+            <input
+              type="text"
+              name="input__cod"
+              id=""
+              onChange={(e) => setCodDoProduto(e.target.value)}
+              required
+            />
           </div>
           <div className="div__alinhamento_campos">
             <label htmlFor="input__num">Num.Pedido:</label>
-            <input type="text" name="input__num" id="" />
+            <input
+              type="text"
+              name="input__num"
+              id=""
+              onChange={(e) => setNumPedido(e.target.value)}
+              required
+            />
           </div>
           <div className="div__alinhamento_campos">
             <label htmlFor="input__ope">Operador:</label>
-            <input type="text" name="input__ope" id="" />
+            <input
+              type="text"
+              name="input__ope"
+              id=""
+              onChange={(e) => setOperador(e.target.value)}
+              required
+            />
           </div>
           <div className="div__alinhamento_campos">
             <label htmlFor="input__sup">Supervisor:</label>
-            <input type="text" name="input__sup" id="" />
+            <input
+              type="text"
+              name="input__sup"
+              id=""
+              onChange={(e) => setSupervisor(e.target.value)}
+              required
+            />
           </div>
           <div className="div__alinhamento_justificativa">
             <label htmlFor="input__jus">Justificativa:</label>
-            <input type="text" name="input__jus" id="" />
+            <input 
+            type="text" 
+            name="input__jus" 
+            id=""
+            onChange={(e) => setJustificativa(e.target.value)}
+              required 
+            />
           </div>
           <button className="botao_lancar" type="submit">
             Lançar
